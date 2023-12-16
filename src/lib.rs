52d7bca682b58f5a9c89b4cee733c8ec2a599368
kahkkk <emmadarkbunny0@gmail.com>
2023-12-16 14:00:50 -0300

    chore: change module name to parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! # 🗃️ Argparse
+//!
+//! `argparse` is a minimal create to parse command line arguments to personal use
+//! and only to learn more about rust
+
 pub mod parser;
 
 pub use parser::*;
--- src/parser.rs
+++ src/parser.rs
@@ -1,18 +1,23 @@
 use std::{collections::HashMap, env};
 
+/// Parser argument struct
 pub struct Argument {
+    /// The name of each argument
     pub name: String,
 }
 
+/// Parser struct itself
 pub struct Parser {
     pub arguments: Vec<Argument>,
 }
 
 impl Parser {
+    /// Create a new parser
     pub fn new() -> Parser {
         Parser { arguments: vec![] }
     }
 
+    /// Add an argument to the parser
     pub fn add_arg(&mut self, name: &str) -> &mut Self {
         self.arguments.push(Argument {
             name: name.to_string(),
@@ -21,6 +26,7 @@ impl Parser {
         self
     }
 
+    /// Parse the arguments based in expected added arguments
     pub fn parse(&self) -> HashMap<String, String> {
         let args: Vec<_> = env::args().skip(1).collect();
 
